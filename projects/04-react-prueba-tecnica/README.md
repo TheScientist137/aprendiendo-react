88b99f590e900c8f227cb1a2438611bcd87b6c40
The-Scientist <thescientist137@outlook.es>
2023-06-01 07:44:10 +0200

    (Primer Objetivo Cumplido)

--- projects/04-react-prueba-tecnica/README.md
+++ projects/04-react-prueba-tecnica/README.md
@@ -3,5 +3,10 @@
 APIs: 
 - Facts Random: https://catfact.ninja/fact
 - Imagen random: https://cataas.com/cat/says/hello
+ - Endopoint for the image: `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
 
-Recupera un hecho aleatorio de gatos de la primera API y muestra una imagen de un gato con la primera palabra del hecho recuperado usando la segunda API.
+Objetivos:
+- Recupera un hecho aleatorio de gatos de la primera API (imprimelo por pantalla) 
+- Recuperar la primera palabra del hecho
+- Muestra una imagen de un gato con la primera palabra
+
--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,3 +1,22 @@
+import { useEffect, useState } from 'react'
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+
 export function App () {
-  return <h1>Hello World!</h1>
+  const [fact, setFact] = useState()
+
+  // Fetching de datos
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(res => res.json())
+      .then(data => setFact(data.fact))
+  }, []) // [] Hacemos el fetching de datos la primera vez que hacemos el fetching de datos
+
+  return (
+    <main>
+      <h1>App de gatitos</h1>
+      {fact && <p>{fact}</p>}
+    </main>
+  )
 }
