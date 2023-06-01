0da382771b70ab37e5776caebe2807bef7fd87b0
The-Scientist <thescientist137@outlook.es>
2023-06-01 09:42:14 +0200

    (Segundo y tercer objetivos cumplidos):
    - Creamos la variable de estado imageUrl
    - Obtenemos las tres primeras palabras del fact y las almacenamos en una variable
    
    - Realizamos el segundo fetching de datos (dentro del .then del primero por lo que dependen el uno del otro)
    - Le pasamos las tres primeras palabras a la url del fetching para obtener la imagen
    - Renderizamos la imagen
    
    - Creamos un archivo App.css
    - Estilizamos la pagina para centrar los elementos

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,22 +1,44 @@
 import { useEffect, useState } from 'react'
+import './App.css'
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
-// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+const CAT_PREFIX_IMAGE = 'https://cataas.com'
 
 export function App () {
   const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
 
+  // Primer Objetivo: Recuperar un random fact de la primera API y muestralo en pantalla
   // Fetching de datos
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then(res => res.json())
-      .then(data => setFact(data.fact))
-  }, []) // [] Hacemos el fetching de datos la primera vez que hacemos el fetching de datos
+      .then(data => {
+        // Accedemos a data y guardamos el hecho en la variable fact
+        const { fact } = data
+        setFact(fact)
+
+        // Segundo Objetivo: Recuperar las tres primeras palabras del hecho
+        // fact.split(' ').slice(0, 2).join(' ')
+        // Otra manera usando el segundo parámetro de split()
+        const firstThreeWords = fact.split(' ', 3).join(' ')
+        console.log(firstThreeWords)
+
+        // Tercer Objetivo: Muestra una imagen de un gato usando la segunda API con las tres primeras palabras recuperadas en el objetivo anterior
+        fetch(`https://cataas.com/cat/says/${firstThreeWords}?size=50&color=red&json=true`)
+          .then(res => res.json())
+          .then(response => {
+            const { url } = response
+            setImageUrl(url)
+          })
+      })
+  }, []) // [] Solo se ejecuta cuando se monta el componente
 
   return (
-    <main>
+    <main className='main'>
       <h1>App de gatitos</h1>
       {fact && <p>{fact}</p>}
+      {imageUrl && <img src={`${CAT_PREFIX_IMAGE}${imageUrl}`} alt={`image extracted using the first three words for: (${fact})`} />}
     </main>
   )
 }
