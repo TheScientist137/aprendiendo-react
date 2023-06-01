afdae43690d02d7ea08de1e48a25c081f26d2822
The-Scientist <thescientist137@outlook.es>
2023-06-01 10:36:10 +0200

    - Separamos la logica en dos useEffect() - Centramos el contenido con display flex en un archivo App.css

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -8,37 +8,49 @@ export function App () {
   const [fact, setFact] = useState()
   const [imageUrl, setImageUrl] = useState()
 
-  // Primer Objetivo: Recuperar un random fact de la primera API y muestralo en pantalla
-  // Fetching de datos
+  // useEffect  para recuperar la cita al recargar la pagina
   useEffect(() => {
+    // Primer Objetivo
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then(res => res.json())
+      // TODO: Handle error if !res.ok
       .then(data => {
         // Accedemos a data y guardamos el hecho en la variable fact
         const { fact } = data
         setFact(fact)
-
-        // Segundo Objetivo: Recuperar las tres primeras palabras del hecho
-        // fact.split(' ').slice(0, 2).join(' ')
-        // Otra manera usando el segundo parámetro de split()
-        const firstThreeWords = fact.split(' ', 3).join(' ')
-        console.log(firstThreeWords)
-
-        // Tercer Objetivo: Muestra una imagen de un gato usando la segunda API con las tres primeras palabras recuperadas en el objetivo anterior
-        fetch(`https://cataas.com/cat/says/${firstThreeWords}?size=50&color=red&json=true`)
-          .then(res => res.json())
-          .then(response => {
-            const { url } = response
-            setImageUrl(url)
-          })
       })
   }, []) // [] Solo se ejecuta cuando se monta el componente
+
+  // useEffect para recuperar la imagen cada vez que tenemos cita nueva
+  useEffect(() => {
+    if (!fact) return
+
+    // Segundo Objetivo:
+    // fact.split(' ').slice(0, 2).join(' ')
+    const firstThreeWords = fact.split(' ', 3).join(' ')
+    console.log(firstThreeWords)
+
+    // Tercer Objetivo
+    fetch(
+      `https://cataas.com/cat/says/${firstThreeWords}?size=50&color=red&json=true`
+    )
+      .then(res => res.json())
+      .then(response => {
+        const { url } = response
+        setImageUrl(url)
+      })
+  }, [fact])
 
   return (
     <main className='main'>
       <h1>App de gatitos</h1>
       {fact && <p>{fact}</p>}
-      {imageUrl && <img src={`${CAT_PREFIX_IMAGE}${imageUrl}`} alt={`image extracted using the first three words for: (${fact})`} />}
+      {imageUrl && (
+        <img
+          src={`${CAT_PREFIX_IMAGE}${imageUrl}`}
+          alt={`image extracted using the first three words for: (${fact})`}
+        />
+      )}
     </main>
   )
 }
